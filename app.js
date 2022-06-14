14bfe0b4d1eb4089725a4ea5b04b8c6464ce5415
GrisGGG <astropoetscapicua@gmail.com>
2022-06-14 01:53:53 -0500

    Añadiendo archivos de configuración

--- app.js
+++ app.js
@@ -1,10 +1,69 @@
 const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Server")
-})
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
-    console.log("Server us runnin on port 3000");
-})
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "8014206db8b17f9a14070c59d0954e69";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query+ "&appid=" + apiKey + "&units=" + unit;
+    
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+        
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + ".png"
+            res.write("<p>The weather is  currently " + weatherDescription + "  </p>")
+            res.write("<h2>The temperature in " + query + " is " + temp + "  </h2>")
+            res.write("<img src=" + imageURL+ ">")
+            res.send()
+   
+        })
+    })
+});
+
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port`);
+  })
+    /*
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+        response.on("data", function (data) {
+
+*/
+            /*
+            const query = "London";
+            const apiKey = "";
+            const unit = "";
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
+            
+
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + ".png"
+            res.write("<p>The weather is  currently " + weatherDescription + "  </p>")
+            res.write("<h2>The temperature in Puebla is " + temp + "  </h2>")
+            res.write("<img src=" + imageURL+ ">")
+            res.send()
+   
+        })
+    });
+   */
+    
+
+
